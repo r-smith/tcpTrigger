4fb3e30fc70bc1cc1070342943e5bff9aa932656
Ryan Smith <ryan@smithlv.com>
2017-01-12 14:13:56 -0800

    Update assembly versions and installer.

--- tcpTrigger.Editor/Properties/AssemblyInfo.cs
+++ tcpTrigger.Editor/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("tcpTrigger Editor")]
-[assembly: AssemblyCopyright("Copyright © 2016 Ryan Smith")]
+[assembly: AssemblyCopyright("Copyright © 2017 Ryan Smith")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
