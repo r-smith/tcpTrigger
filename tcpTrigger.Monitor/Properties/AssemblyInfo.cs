56560c86900535988221b5dabe9b16d6f9800290
Ryan Smith <R-Smith@users.noreply.github.com>
2021-08-16 15:07:25 -0700

    Update assembly info

--- tcpTrigger.Monitor/Properties/AssemblyInfo.cs
+++ tcpTrigger.Monitor/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("tcpTrigger.Monitor")]
+[assembly: AssemblyTitle("tcpTrigger Monitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("tcpTrigger.Monitor")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("tcpTrigger Monitor")]
+[assembly: AssemblyCopyright("Copyright © 2021 Ryan Smith")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
