d342fe4d672767f8cdccd732b0feb5542729dd31
Ryan Smith <R-Smith@users.noreply.github.com>
2021-08-28 23:04:12 -0700

    Add settings class

---
+++ tcpTrigger.Monitor/Classes/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace tcpTrigger.Monitor
+{
+    public static class Settings
+    {
+        public static bool ExitToTray { get; private set; } = false;
+        public static bool MinimizeToTray { get; private set; } = false;
+        public static bool FocusOnUpdate { get; private set; } = true;
+        public static bool LaunchAtLogon { get; private set; } = false;
+    }
+}
