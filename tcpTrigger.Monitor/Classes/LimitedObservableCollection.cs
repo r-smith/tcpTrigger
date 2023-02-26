3aed076c64090531672fcd1cb7c5e255d3420d36
Ryan Smith <R-Smith@users.noreply.github.com>
2023-02-25 18:43:19 -0800

    Add limited observable collection class

---
+++ tcpTrigger.Monitor/Classes/LimitedObservableCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace tcpTrigger.Monitor
+{
+    public class LimitedObservableCollection<T> : ObservableCollection<T>
+    {
+        public int Capacity { get; }
+
+        public LimitedObservableCollection(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public new void Insert(int index, T item)
+        {
+            if (Count >= Capacity)
+            {
+                RemoveAt(Count - 1);
+            }
+            base.Insert(index, item);
+        }
+    }
+}
